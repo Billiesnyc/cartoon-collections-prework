f84adc9b21ffbbdac919348d7d2740036868870f
Billie Simmons <billiesnyc@gmail.com>
2018-08-16 11:29:31 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ end
 def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
   result = cheese & cheese_types
-  if result.lenght > 1
+  if result.length > 1
   result.join()
 else 
   nil 
