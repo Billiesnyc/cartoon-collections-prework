d82b583b163a9bda45688895adcbac26de9f6453
Billie Simmons <billiesnyc@gmail.com>
2018-08-16 11:22:47 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,6 +20,6 @@ end
 def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese.find do |item| 
-    item == "cheddar" or "gouda" or "camembert"
+    item == cheese_types
   end
 end
